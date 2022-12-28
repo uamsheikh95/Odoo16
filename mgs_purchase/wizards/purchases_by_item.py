1b1b5fe9825a31edfe62d92ae56e2ed4bd619ff1
uamsheikh95 <uamsheikh95@gmail.com>
2022-12-28 10:53:46 +0300

    Remmitance mutli-currency feature

--- mgs_purchase/wizards/purchases_by_item.py
+++ mgs_purchase/wizards/purchases_by_item.py
@@ -1,5 +1,8 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+import xlsxwriter
+import base64
+from io import BytesIO
 
 
 class PurchasesbyItemDetail(models.TransientModel):
@@ -14,6 +17,8 @@ class PurchasesbyItemDetail(models.TransientModel):
         'res.company', string='Company', default=lambda self: self.env.user.company_id.id)
     report_by = fields.Selection(
         [('Summary', 'Summary'), ('Detail', 'Detail')], string='Report Type', default='Detail')
+    datas = fields.Binary('File', readonly=True)
+    datas_fname = fields.Char('Filename', readonly=True)
 
     @api.constrains('date_from', 'date_to')
     def _check_the_date_from_and_to(self):
@@ -34,6 +39,200 @@ class PurchasesbyItemDetail(models.TransientModel):
         }
 
         return self.env.ref('mgs_purchase.action_purchases_by_item').report_action(self, data=data)
+
+    def export_to_excel(self):
+        purchases_by_item_report_obj = self.env['report.mgs_purchase.purchases_by_item_report']
+        lines = purchases_by_item_report_obj._lines
+
+        fp = BytesIO()
+        workbook = xlsxwriter.Workbook(fp)
+        filename = 'PurchasesByItem'
+        worksheet = workbook.add_worksheet(filename)
+
+        heading_format = workbook.add_format(
+            {'align': 'center', 'valign': 'vcenter', 'bold': True, 'size': 14})
+        sub_heading_format = workbook.add_format(
+            {'align': 'center', 'valign': 'vcenter', 'bold': True, 'size': 12})
+        cell_text_format = workbook.add_format(
+            {'align': 'left', 'bold': True, 'size': 12})
+        cell_number_format = workbook.add_format(
+            {'align': 'right', 'bold': True, 'size': 12})
+        align_right = workbook.add_format(
+            {'align': 'right', 'num_format': '#,##0.00'})
+        align_right_money = workbook.add_format(
+            {'align': 'right', 'num_format': '$#,##0.00'})
+        align_right_money_total = workbook.add_format(
+            {'align': 'right', 'bold': True, 'num_format': '$#,##0.00'})
+        align_right_total = workbook.add_format(
+            {'align': 'right', 'bold': True, 'num_format': '#,##0.00'})
+        date_heading_format = workbook.add_format(
+            {'align': 'left', 'bold': True, 'size': 12, 'num_format': 'd-m-yyyy'})
+        date_format = workbook.add_format(
+            {'align': 'left', 'num_format': 'd-m-yyyy'})
+
+        # Heading
+        row = 1
+        worksheet.merge_range(
+            'A1:I1', self.company_id.name, sub_heading_format)
+        row += 1
+        worksheet.merge_range('A2:I3', 'Purchases by Item', heading_format)
+
+        # Search criteria
+        row += 2
+        column = -1
+        if self.date_from:
+            row += 1
+            worksheet.write(row, column+1, 'From Date', cell_text_format)
+            worksheet.write(row, column+2, self.date_from or '',
+                            date_heading_format)
+        column+2
+
+        if self.date_to:
+            row += 1
+            worksheet.write(row, column+1, 'To Date', cell_text_format)
+            worksheet.write(row, column+2, self.date_to or '',
+                            date_heading_format)
+        column+2
+
+        if self.product_id:
+            row += 1
+            worksheet.write(row, column+1, 'Product', cell_text_format)
+            worksheet.write(row, column+2, self.product_id.name or '')
+        column+2
+
+        # Sub headers
+        row += 2
+        column = -1
+        worksheet.write(row, column+1, 'Item', cell_text_format)
+
+        worksheet.write(row, column+2, 'Ordered Qty', cell_number_format)
+        worksheet.write(row, column+3, 'Delivered Qty', cell_number_format)
+        worksheet.write(row, column+4, 'Invoiced Qty', cell_number_format)
+        worksheet.write(row, column+5, 'Amount', cell_number_format)
+
+        if self.env.user.has_group('account.group_account_manager'):
+            worksheet.write(row, column+6, 'T.Cost', cell_number_format)
+            worksheet.write(row, column+7, 'Gross Profit', cell_number_format)
+
+        if self.report_by == 'Detail':
+            worksheet.write(row, column+2, 'Date', cell_text_format)
+            worksheet.write(row, column+3, 'Order', cell_text_format)
+            worksheet.write(row, column+4, 'Vendor', cell_text_format)
+
+            worksheet.write(row, column+5, 'Ordered Qty', cell_number_format)
+            worksheet.write(row, column+6, 'Received Qty', cell_number_format)
+            worksheet.write(row, column+7, 'Billed Qty', cell_number_format)
+
+            worksheet.write(row, column+8, 'Rate', cell_number_format)
+            worksheet.write(row, column+9, 'Amount', cell_number_format)
+
+        # Lines
+        for main in lines(self.date_from, self.date_to, self.company_id.id, self.product_id.id, 'all'):
+            # ------------------------------ Item ------------------------------
+
+            for product in lines(self.date_from, self.date_to, self.company_id.id, self.product_id.id, 'yes'):
+
+                if self.report_by == 'Summary':
+                    row += 1
+                    column = -1
+                    worksheet.write(row, column+1, product['product_name'])
+                    worksheet.write(
+                        row, column+2, "{:,}".format(product['total_qty_ordered']), align_right)
+                    worksheet.write(
+                        row, column+3, "{:,}".format(product['total_qty_received']), align_right)
+                    worksheet.write(
+                        row, column+4, "{:,}".format(product['total_qty_billed']), align_right)
+                    worksheet.write(
+                        row, column+5, "{:,}".format(product['total_amount']), align_right_money)
+
+                if self.report_by == 'Detail':
+                    row += 2
+                    column = -1
+                    row_number = 'A%s:K%s' % (row, row)
+                    worksheet.merge_range(
+                        row_number, product['product_name'], cell_text_format)
+
+                    # ------------------------------ Lines ------------------------------
+                    for line in lines(self.date_from, self.date_to, self.company_id.id, product['product_id'], 'no'):
+                        row += 1
+                        column = -1
+
+                        worksheet.write(row, column+1, '')
+                        worksheet.write(
+                            row, column+2, line['date_order'], date_format)
+                        worksheet.write(row, column+3, line['order_no'])
+                        worksheet.write(row, column+4, line['partner'])
+                        worksheet.write(
+                            row, column+5, "{:,}".format(line['qty_ordered']), align_right)
+                        worksheet.write(
+                            row, column+6, "{:,}".format(line['qty_received']), align_right)
+                        worksheet.write(
+                            row, column+7, "{:,}".format(line['qty_billed']), align_right)
+
+                        worksheet.write(
+                            row, column+8, line['price_total']/line['qty_ordered'], align_right)
+                        worksheet.write(
+                            row, column+9, "{:,}".format(line['price_total']), align_right_money)
+
+                        # ---------------------------------------- END LINES ----------------------------------------
+
+                    row += 2
+                    column = -1
+                    worksheet.write(row, column+1, 'TOTAL ' +
+                                    product['product_name'], cell_text_format)
+                    worksheet.write(row, column+2, '', cell_text_format)
+                    worksheet.write(row, column+3, '', cell_text_format)
+                    worksheet.write(row, column+4, '', cell_text_format)
+                    worksheet.write(
+                        row, column+5, "{:,}".format(product['total_qty_ordered']), align_right_total)
+                    worksheet.write(
+                        row, column+6, "{:,}".format(product['total_qty_received']), align_right_total)
+                    worksheet.write(
+                        row, column+7, "{:,}".format(product['total_qty_billed']), align_right_total)
+                    worksheet.write(row, column+8, '')
+                    worksheet.write(
+                        row, column+9, "{:,}".format(product['total_amount']), align_right_money_total)
+
+            # Main Totals
+            row += 2
+            column = -1
+            worksheet.write(row, column+1, 'Total', cell_text_format)
+
+            worksheet.write(
+                row, column+2, "{:,}".format(main['total_qty_ordered_all']), align_right_total)
+            worksheet.write(
+                row, column+3, "{:,}".format(main['total_qty_received_all']), align_right_total)
+            worksheet.write(
+                row, column+4, "{:,}".format(main['total_qty_billed_all']), align_right_total)
+            worksheet.write(
+                row, column+5, "{:,}".format(main['total_amount_all']), align_right_money_total)
+
+            if self.report_by == 'Detail':
+                worksheet.write(row, column+2, '', cell_text_format)
+                worksheet.write(row, column+3, '', cell_text_format)
+                worksheet.write(row, column+4, '', cell_text_format)
+
+                worksheet.write(
+                    row, column+5, "{:,}".format(main['total_qty_ordered_all']), align_right_total)
+                worksheet.write(
+                    row, column+6, "{:,}".format(main['total_qty_received_all']), align_right_total)
+                worksheet.write(
+                    row, column+7, "{:,}".format(main['total_qty_billed_all']), align_right_total)
+                worksheet.write(row, column+8, '', cell_number_format)
+                worksheet.write(
+                    row, column+9, "{:,}".format(main['total_amount_all']), align_right_money_total)
+
+        workbook.close()
+        out = base64.encodebytes(fp.getvalue())
+        self.write({'datas': out, 'datas_fname': filename})
+        fp.close()
+        filename += '%2Exlsx'
+
+        return {
+            'type': 'ir.actions.act_url',
+            'target': 'new',
+            'url': 'web/content/?model='+self._name+'&id='+str(self.id)+'&field=datas&download=true&filename='+filename,
+        }
 
 
 class PurchasesbyItemDetailReport(models.AbstractModel):
